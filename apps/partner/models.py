070b96f715dab6e0bb04d908ae60ffc3e290838c
GodMode-On <yura.hlyan@gmail.com>
2017-02-06 19:32:08 +0200

    Made fields 'partner' and 'partner_sku' not unique together

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -10,5 +10,11 @@ class StockRecord(AbstractStockRecord):
         _("Sale price"), decimal_places=2, max_digits=12,
         blank=True, null=True)
 
+    class Meta:
+        app_label = 'partner'
+        unique_together = ()
+        verbose_name = _("Stock record")
+        verbose_name_plural = _("Stock records")
+
 
 from oscar.apps.partner.models import *  # noqa
