cc456bc86f93d71b88b9d11bdb8421b3dc011447
GodMode-On <yura.hlyan@gmail.com>
2017-02-09 22:40:10 +0200

    Adde presentation to product

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from oscar.apps.catalogue.abstract_models import AbstractProduct
+
+
+class Product(AbstractProduct):
+    presentation = models.CharField(
+        _("Presentation"), max_length=64, blank=True, null=True)
+
 from oscar.apps.catalogue.models import *  # noqa
