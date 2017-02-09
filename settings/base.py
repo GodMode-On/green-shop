1030e4d5752bbe9cbbe6883284e60c91a36d983d
GodMode-On <yura.hlyan@gmail.com>
2017-02-09 22:12:25 +0200

    Forked Oscar's basket

--- settings/base.py
+++ settings/base.py
@@ -244,6 +244,7 @@ INSTALLED_APPS = [
 ] + oscar.get_core_apps([
     'apps.partner',
     'apps.dashboard.catalogue',
+    'apps.basket',
 ])
 
 # Add Oscar's custom auth backend so users can sign in using their email
