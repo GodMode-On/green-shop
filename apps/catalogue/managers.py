34eff2970dfd7aa211f756c6c8dea172f56fcacf
GodMode-On <yura.hlyan@gmail.com>
2017-02-10 22:58:26 +0200

    Fixed managers

--- apps/catalogue/managers.py
+++ apps/catalogue/managers.py
@@ -12,13 +12,6 @@ class ProductQuerySet(oscar_managers.ProductQuerySet):
 
 
 class ProductManager(models.Manager):
-    """
-    Uses ProductQuerySet and proxies its methods to allow chaining
-
-    Once Django 1.7 lands, this class can probably be removed:
-    https://docs.djangoproject.com/en/dev/releases/1.7/#calling-custom-queryset-methods-from-the-manager  # noqa
-    """
-
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
 
@@ -30,11 +23,5 @@ class ProductManager(models.Manager):
 
 
 class BrowsableProductManager(ProductManager):
-    """
-    Excludes non-canonical products
-
-    Could be deprecated after Oscar 0.7 is released
-    """
-
     def get_queryset(self):
         return super(BrowsableProductManager, self).get_queryset().browsable()
