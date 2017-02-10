7eff6816c3449833a722cbe23796000cecbaf5d2
GodMode-On <yura.hlyan@gmail.com>
2017-02-10 22:34:51 +0200

    Added method for product selecting dropdown

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -8,4 +8,13 @@ class Product(AbstractProduct):
     presentation = models.CharField(
         _("Presentation"), max_length=64, blank=True, null=True)
 
+    @property
+    def get_title_for_dropdown(self):
+        if self.presentation:
+            return self.presentation
+        elif self.title:
+            return self.title
+        else:
+            return _("Product {}".format(self.pk))
+
 from oscar.apps.catalogue.models import *  # noqa
