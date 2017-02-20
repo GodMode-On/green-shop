ee09a2042db391864bacd457da9950f8e8b90c69
GodMode-On <yura.hlyan@gmail.com>
2017-02-20 23:41:07 +0200

    Hide some features on dashboard

--- settings/base.py
+++ settings/base.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 from .sensitive.secret_key import SECRET_KEY
 from .sensitive.settings_postgres import DATABASES
 
@@ -325,6 +327,126 @@ OSCAR_DEFAULT_CURRENCY = 'UAH'
 DISPLAY_VERSION = False
 
 OSCAR_HIDDEN_FEATURES = ['reviews']
+
+# Menu structure of the dashboard navigation
+OSCAR_DASHBOARD_NAVIGATION = [
+    {
+        'label': _('Dashboard'),
+        'icon': 'icon-th-list',
+        'url_name': 'dashboard:index',
+    },
+    {
+        'label': _('Catalogue'),
+        'icon': 'icon-sitemap',
+        'children': [
+            {
+                'label': _('Products'),
+                'url_name': 'dashboard:catalogue-product-list',
+            },
+            {
+                'label': _('Product Types'),
+                'url_name': 'dashboard:catalogue-class-list',
+            },
+            {
+                'label': _('Categories'),
+                'url_name': 'dashboard:catalogue-category-list',
+            },
+            # {
+            #     'label': _('Ranges'),
+            #     'url_name': 'dashboard:range-list',
+            # },
+            # {
+            #     'label': _('Low stock alerts'),
+            #     'url_name': 'dashboard:stock-alert-list',
+            # },
+        ]
+    },
+    {
+        'label': _('Fulfilment'),
+        'icon': 'icon-shopping-cart',
+        'children': [
+            {
+                'label': _('Orders'),
+                'url_name': 'dashboard:order-list',
+            },
+            {
+                'label': _('Statistics'),
+                'url_name': 'dashboard:order-stats',
+            },
+            # {
+            #     'label': _('Partners'),
+            #     'url_name': 'dashboard:partner-list',
+            # },
+            # The shipping method dashboard is disabled by default as it might
+            # be confusing. Weight-based shipping methods aren't hooked into
+            # the shipping repository by default (as it would make
+            # customising the repository slightly more difficult).
+            # {
+            #     'label': _('Shipping charges'),
+            #     'url_name': 'dashboard:shipping-method-list',
+            # },
+        ]
+    },
+    {
+        'label': _('Customers'),
+        'icon': 'icon-group',
+        'children': [
+            {
+                'label': _('Customers'),
+                'url_name': 'dashboard:users-index',
+            },
+            # {
+            #     'label': _('Stock alert requests'),
+            #     'url_name': 'dashboard:user-alert-list',
+            # },
+        ]
+    },
+    # {
+    #     'label': _('Offers'),
+    #     'icon': 'icon-bullhorn',
+    #     'children': [
+    #         {
+    #             'label': _('Offers'),
+    #             'url_name': 'dashboard:offer-list',
+    #         },
+    #         {
+    #             'label': _('Vouchers'),
+    #             'url_name': 'dashboard:voucher-list',
+    #         },
+    #     ],
+    # },
+    {
+        'label': _('Content'),
+        'icon': 'icon-folder-close',
+        'children': [
+            {
+                'label': _('Content blocks'),
+                'url_name': 'dashboard:promotion-list',
+            },
+            {
+                'label': _('Content blocks by page'),
+                'url_name': 'dashboard:promotion-list-by-page',
+            },
+            {
+                'label': _('Pages'),
+                'url_name': 'dashboard:page-list',
+            },
+            # {
+            #     'label': _('Email templates'),
+            #     'url_name': 'dashboard:comms-list',
+            # },
+            # {
+            #     'label': _('Reviews'),
+            #     'url_name': 'dashboard:reviews-list',
+            # },
+        ]
+    },
+    {
+        'label': _('Reports'),
+        'icon': 'icon-bar-chart',
+        'url_name': 'dashboard:reports-index',
+    },
+]
 
 
 # Order processing
