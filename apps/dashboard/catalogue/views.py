29de4cbd2433586a1bf9ff59935dc3645f8c79d1
GodMode-On <yura.hlyan@gmail.com>
2017-02-21 21:43:30 +0200

    Ensure that child products will get ProductClass and Categories from their parents

--- apps/dashboard/catalogue/views.py
+++ apps/dashboard/catalogue/views.py
@@ -1,6 +1,6 @@
 from oscar.apps.dashboard.catalogue import views as oscar_views
 
-from apps.catalogue.models import Product
+from apps.catalogue.models import Product, ProductCategory
 
 
 class ProductListView(oscar_views.ProductListView):
@@ -12,3 +12,18 @@ class ProductListView(oscar_views.ProductListView):
         queryset = self.filter_queryset(queryset)
         queryset = self.apply_search(queryset)
         return queryset
+
+
+class ProductCreateUpdateView(oscar_views.ProductCreateUpdateView):
+    def forms_valid(self, form, formsets):
+        response = super(
+            ProductCreateUpdateView, self).forms_valid(form, formsets)
+
+        if self.object.is_child:
+            self.object.product_class = self.object.parent.product_class
+            for cat in self.object.parent.categories.all():
+                ProductCategory.objects.get_or_create(
+                    product=self.object, category=cat)
+
+        self.object.save()
+        return response
