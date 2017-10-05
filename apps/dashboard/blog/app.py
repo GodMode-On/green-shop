42d7153c727b3d8a3658b8ccf9b1dc3e9d7b9f76
GodMode-On <yura.hlyan@gmail.com>
2017-10-05 22:10:33 +0300

    Added creating posts

--- apps/dashboard/blog/app.py
+++ apps/dashboard/blog/app.py
@@ -2,16 +2,20 @@ from django.conf.urls import url
 
 from oscar.core.application import DashboardApplication
 
-from apps.dashboard.blog.views import PostsListView
+from apps.dashboard.blog import views
 
 
 class CommsDashboardApplication(DashboardApplication):
-    name = 'blog'
+    name = None
     default_permissions = ['is_staff', ]
 
     def get_urls(self):
         urls = [
-            url(r'^$', PostsListView.as_view(), name='posts-list'),
+            url(r'^$', views.PostsListView.as_view(), name='posts-list'),
+            url(
+                r'^post_create/$',
+                views.PostCreateView.as_view(),
+                name='post-create'),
         ]
         return self.post_process_urls(urls)
 
--- apps/dashboard/blog/views.py
+++ apps/dashboard/blog/views.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from django.views import generic
+from django.core.urlresolvers import reverse
 
 from apps.blog import models
+from apps.dashboard.blog import forms
 
 
 class PostsListView(generic.ListView):
@@ -9,3 +13,17 @@ class PostsListView(generic.ListView):
     paginate_by = 10
     context_object_name = 'posts'
     ordering = '-date_added'
+
+
+class PostCreateView(generic.CreateView):
+    template_name = 'dashboard/blog/post_create_update.html'
+    model = models.Post
+    form_class = forms.PostCreateUpdateForm
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(PostCreateView, self).get_context_data(*args, **kwargs)
+        ctx['page_title'] = "Створити"
+        return ctx
+
+    def get_success_url(self):
+        return reverse('dashboard:posts-list')
