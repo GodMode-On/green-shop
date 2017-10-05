495df50da7c70f49c022b6181a97261b0559716f
GodMode-On <yura.hlyan@gmail.com>
2017-10-05 21:34:56 +0300

    Added list of posts to dahboard

--- apps/dashboard/blog/views.py
+++ apps/dashboard/blog/views.py
@@ -5,7 +5,7 @@ from apps.blog import models
 
 class PostsListView(generic.ListView):
     model = models.Post
-    template_name = 'dashboard/qa/questions_list.html'
+    template_name = 'dashboard/blog/posts_list.html'
     paginate_by = 10
     context_object_name = 'posts'
     ordering = '-date_added'
