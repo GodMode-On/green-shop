1f75cb236b2e68286a033b051095eeea4c258270
GodMode-On <yura.hlyan@gmail.com>
2017-10-05 22:00:47 +0300

    Renamed field in post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField("Назва", max_length=256)
-    text = models.TextField("Текст")
+    post_text = models.TextField("Текст")
     date_added = models.DateTimeField("Дата", auto_now_add=True)
 
     def __unicode__(self):
